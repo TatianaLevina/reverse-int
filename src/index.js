83c853a806adbb5e5fa3e2029bb391a34f5adb51
Tatiana Levina <tatiana.levina.v@gmail.com>
2023-01-09 23:28:22 +0300

    feat: create function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
-}
+    n = n.toString();
+    let result = '';
+    if (n[0] === '-'){
+      result = n[0];
+      for (let i = 1; i < n.length; i++) {
+        result = `${n[i]}${result}`;
+      }
+    } else{
+      for (let i = 0; i < n.length; i++) {
+        result = `${n[i]}${result}`;
+      }
+    }
+    return parseInt(result);
+  };
